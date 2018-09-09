a0d856126292e11e5d6046f9d2e70f6ab452d8a7
fvalka <fvalka@vektorraum.com>
2018-09-09 10:58:13 +0200

    International Sigmets are now plotted correctly

--- main.py
+++ main.py
@@ -1,23 +1,30 @@
+# ----------------------------------------------------------------------
+# ------------------------- IMPORT LIBRARIES ---------------------------
 import json  # reading geojson files
-from urllib.request import urlopen
-
 import matplotlib.pyplot as plt  # plotting data
-from adjustText import adjust_text
-from descartes import PolygonPatch  # integrating geom object to matplot
-from matplotlib.collections import PatchCollection
-from mpl_toolkits.basemap import Basemap
-from shapely.geometry import asShape, box, Point  # manipulating geometry
+import math
+import random
+import numpy as np
 from shapely.ops import transform
+from matplotlib.collections import PatchCollection
+from shapely.geometry import asShape, box, Point, LineString  # manipulating geometry
+from descartes import PolygonPatch  # integrating geom object to matplot
+from mpl_toolkits.basemap import Basemap
+from urllib.request import urlopen
+from adjustText import adjust_text
 
-# ------------------------- LOAD THE DATA -----------------------------
-# data = json.load(open("sigmet.json")) # from data folder.
 
-response = urlopen("https://www.aviationweather.gov/gis/scripts/IsigmetJSON.php")
-response_decoded = response.read().decode("utf-8")
-data = json.loads(response_decoded)
+def load_json_from_web(url):
+    response = urlopen(url)
+    response_decoded = response.read().decode("utf-8")
+    return json.loads(response_decoded)
+
+
+sigmets_international = load_json_from_web("https://www.aviationweather.gov/gis/scripts/IsigmetJSON.php")
+sigmets_us = load_json_from_web("https://www.aviationweather.gov/gis/scripts/SigmetJSON.php")
 
 # initiate the plot axes
-fig = plt.figure(figsize=(16, 16))  # create a figure to contain the plot elements
+fig = plt.figure(figsize=(12, 12))  # create a figure to contain the plot elements
 ax = fig.gca()
 
 # map = Basemap(projection='cyl', urcrnrlon=44, urcrnrlat=72, llcrnrlon=-19, llcrnrlat=33, resolution="i")
@@ -26,8 +33,8 @@ ax = fig.gca()
 map = Basemap(llcrnrlon=-15, llcrnrlat=30, urcrnrlon=46, urcrnrlat=67,
               projection='lcc', lat_1=40, lat_2=55, lon_0=10, resolution="i")
 # North America
-# map = Basemap(llcrnrlon=-130,llcrnrlat=5,urcrnrlon=-55,urcrnrlat=70,
-#              projection='cyl',lat_1=40,lat_2=55,lon_0=-100, resolution="c")
+map = Basemap(llcrnrlon=-130, llcrnrlat=5, urcrnrlon=-55, urcrnrlat=70,
+              projection='cyl', lat_1=40, lat_2=55, lon_0=-100, resolution="c")
 map.drawcoastlines(color='#00BCD1')
 map.drawcountries(color='#4E4D4A')
 map.drawmapboundary(fill_color='#B0F8FF')
@@ -36,36 +43,37 @@ map.fillcontinents(color='#FEF9F0', lake_color='#B0F8FF')
 regionBox = box(minx=map.xmin, miny=map.ymin, maxx=map.xmax, maxy=map.ymax)
 
 # loop through the features plotting polygon centroid
-patches = []
 info = []
 texts = []
 
-for feat in data["features"]:
-    if feat["geometry"]["type"] == "Polygon":
-        # convert the geometry to shapely
-        geom = asShape(feat["geometry"])
-        # obtain the coordinates of the feature's centroid
-        x, y = geom.centroid.x, geom.centroid.y
-        x_conv, y_conv = map(x, y)
-        centroid_conv = Point(x_conv, y_conv)
 
-        geom_conv = transform(map, geom)
+def plot_features(features, label_property, text_property):
+    patches = []
+    for feat in features:
+        if feat["geometry"]["type"] == "Polygon":
+            # convert the geometry to shapely
+            geom = transform(map, asShape(feat["geometry"]))
+            # obtain the coordinates of the feature's centroid
+            centroid = geom.centroid
 
-        patches.append(PolygonPatch(geom_conv))
+            patches.append(PolygonPatch(geom))
 
-        if regionBox.contains(centroid_conv):
-            idx = len(info)
-            text = feat["properties"]["hazard"] + "\n" + str(idx) + "."
-            new_text = ax.text(x_conv, y_conv, text, horizontalalignment='center',
-                               verticalalignment='center', zorder=5, fontweight="heavy", fontsize=10)
-            texts.append(new_text)
-            info.append(feat["properties"]["rawSigmet"])
+            if geom.intersects(regionBox):
+                idx = len(info)
+                text = feat["properties"][label_property] + "\n" + str(idx) + "."
+                new_text = ax.text(centroid.x, centroid.y, text, horizontalalignment='center',
+                                   verticalalignment='center', zorder=5, fontweight="heavy", fontsize=9)
+                texts.append(new_text)
+                info.append(feat["properties"][text_property])
 
-ax.add_collection(
-    PatchCollection(patches, facecolor='#D40F67', edgecolor='#D40F67', linewidths=1.5, alpha=0.3, zorder=4))
+    ax.add_collection(
+        PatchCollection(patches, facecolor='#D40F67', edgecolor='#D40F67', linewidths=1.5, alpha=0.3, zorder=4))
+
+
+plot_features(sigmets_international["features"], "hazard", "rawSigmet")
+
 adjust_text(texts, ha='center', va='center', expand_text=(1.05, 1), autoalign=False,
-            on_basemap=True, text_from_points=False)
+            on_basemap=True, text_from_points=False, arrowprops=dict(arrowstyle='->', color='red'))
 
-ax.clear()  # clear the axes memory
 # plt.savefig("data\siaya_wards.png")
 plt.show()
