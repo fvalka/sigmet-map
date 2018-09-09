1348254103b36b9dfb0160138e4c8dc65f61f508
fvalka <fvalka@vektorraum.com>
2018-09-09 11:49:44 +0200

    Added metars to plot

--- main.py
+++ main.py
@@ -20,8 +20,6 @@ def load_json_from_web(url):
     return json.loads(response_decoded)
 
 
-sigmets_international = load_json_from_web("https://www.aviationweather.gov/gis/scripts/IsigmetJSON.php")
-sigmets_us = load_json_from_web("https://www.aviationweather.gov/gis/scripts/SigmetJSON.php")
 
 # initiate the plot axes
 fig = plt.figure(figsize=(12, 12))  # create a figure to contain the plot elements
@@ -34,13 +32,19 @@ map = Basemap(llcrnrlon=-15, llcrnrlat=30, urcrnrlon=46, urcrnrlat=67,
               projection='lcc', lat_1=40, lat_2=55, lon_0=10, resolution="i")
 # North America
 map = Basemap(llcrnrlon=-130, llcrnrlat=5, urcrnrlon=-55, urcrnrlat=70,
-              projection='cyl', lat_1=40, lat_2=55, lon_0=-100, resolution="c")
+              projection='cyl', lat_1=40, lat_2=55, lon_0=-100, resolution="i")
 map.drawcoastlines(color='#00BCD1')
 map.drawcountries(color='#4E4D4A')
 map.drawmapboundary(fill_color='#B0F8FF')
 map.fillcontinents(color='#FEF9F0', lake_color='#B0F8FF')
 
 regionBox = box(minx=map.xmin, miny=map.ymin, maxx=map.xmax, maxy=map.ymax)
+
+sigmets_international = load_json_from_web("https://www.aviationweather.gov/gis/scripts/IsigmetJSON.php")
+sigmets_us = load_json_from_web("https://www.aviationweather.gov/gis/scripts/SigmetJSON.php")
+
+bbox = str(map.lonmin) + "," + str(map.latmin) + "," + str(map.lonmax) + "," + str(map.latmax)
+metars = load_json_from_web("https://www.aviationweather.gov/gis/scripts/MetarJSON.php?density=all&bbox=" + bbox)
 
 # loop through the features plotting polygon centroid
 info = []
@@ -62,15 +66,29 @@ def plot_features(features, label_property, text_property):
                 idx = len(info)
                 text = feat["properties"][label_property] + "\n" + str(idx) + "."
                 new_text = ax.text(centroid.x, centroid.y, text, horizontalalignment='center',
-                                   verticalalignment='center', zorder=5, fontweight="heavy", fontsize=9)
+                                   verticalalignment='center', zorder=50, fontweight="heavy", fontsize=9)
                 texts.append(new_text)
                 info.append(feat["properties"][text_property])
 
     ax.add_collection(
-        PatchCollection(patches, facecolor='#D40F67', edgecolor='#D40F67', linewidths=1.5, alpha=0.3, zorder=4))
+        PatchCollection(patches, facecolor='#D40F67', edgecolor='#D40F67', linewidths=1.5, alpha=0.3, zorder=40))
+
+
+def plot_metars(metar_features):
+    for feat in metar_features:
+        if feat["geometry"]["type"] == "Point":
+            geom = transform(map, Point(feat["geometry"]["coordinates"][0], feat["geometry"]["coordinates"][1]))
+
+            flight_cat_colors = { "VFR": "#87F70F", "MVFR": "#046D8B", "IFR": "#FF3D7F", "LIFR": "#8808BB"}
+            color = flight_cat_colors.get(feat["properties"].get("fltcat", "?"), "#DAD6CE")
+
+            ax.plot(geom.x, geom.y, 'o', color=color, markersize=6, zorder=30, alpha=0.5)
 
 
 plot_features(sigmets_international["features"], "hazard", "rawSigmet")
+plot_features(sigmets_us["features"], "hazard", "rawAirSigmet")
+
+plot_metars(metars["features"])
 
 adjust_text(texts, ha='center', va='center', expand_text=(1.05, 1), autoalign=False,
             on_basemap=True, text_from_points=False, arrowprops=dict(arrowstyle='->', color='red'))
