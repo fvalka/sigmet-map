36013cc1c82c0a22783caf3e6c31e537464d2e4f
fvalka <fvalka@vektorraum.com>
2018-09-16 22:16:11 +0200

    Added clean up job which deletes the old plot results

--- sigmet_map_http.py
+++ sigmet_map_http.py
@@ -1,20 +1,30 @@
 import logging
+import os
 import secrets
+import time
 
 from flask import Flask, jsonify, url_for, abort
 from flask_caching import Cache
+from apscheduler.schedulers.background import BackgroundScheduler
 
 # CONSTANTS/CONFIGURATION
 from sigmet_map import MapProvider, FeatureProvider, SigmetMap
 
 logging.basicConfig(level=logging.DEBUG)
 
+# Flask Setup
 app = Flask(__name__)
 cache = Cache(app, config={'CACHE_TYPE': 'simple'})
 
+# Application Setup
 map_provider = MapProvider()
 feature_provider = FeatureProvider()
 sigmet_map_plotter = SigmetMap(map_provider, feature_provider)
+plots_dir = "static/"
+
+# Background Task Setup
+sched = BackgroundScheduler()
+sched.start()
 
 
 @app.route('/')
@@ -30,9 +40,18 @@ def sigmet_map(region):
 
     # Use a random file path, which should never ever collide
     file_name = secrets.token_urlsafe(32) + ".png"
-    plot_result = sigmet_map_plotter.plot(region, "static/" + file_name)
+    plot_result = sigmet_map_plotter.plot(region, plots_dir + file_name)
     url = url_for('static', filename=file_name)
     return jsonify(url=url, infos=plot_result.info, failed=plot_result.failed)
+
+
+@sched.scheduled_job(trigger='interval', minutes=2)
+def cleanup():
+    now = time.time()
+    for file in os.listdir(plots_dir):
+        created = os.path.getctime(plots_dir + file)
+        if (now - created) >= 60*60:
+            os.unlink(plots_dir + file)
 
 
 if __name__ == '__main__':
